a58ccc4a8e78ae3eebb81eb08560a1db00a14256
apmason1210 <alexmason1210@gmail.com>
2026-07-20 13:46:56 +0100

    Written and tested Mosaic to new DEM function
    Written and tested Mosaic to New Raster function to take in the LiDAR DTM 2m tiles and output as single new DTM raster dataset as TIF.

--- EGM722_AlexMason_B01039763_HLS_RAG_Assessment.py
+++ EGM722_AlexMason_B01039763_HLS_RAG_Assessment.py
@@ -4,24 +4,71 @@ import arcpy
 from arcpy import env  
 from arcpy.sa import *
 
-#Set processing environment, matching the extent of the input LULC raster tile.
-env.workspace = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\InputDataLULC"
+def LULC_Reclassify():
+    #Set processing environment, matching the folder location of the input LULC raster tile.
+    env.workspace = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\InputDataLULC"
 
-# Set local variables:
-#Set location of the input LULC dataset.
-inRasterLULC = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\InputDataLULC\LCM.tif"
+    # Set local variables:
+    # Set location of the input LULC dataset.
+    inRasterLULC = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\InputDataLULC\LCM.tif"
 
-# Set the field or band of the raster which holds the numerical land classes.
-reclassFieldLULC = "Value"
+    # Set the field or band of the raster which holds the numerical land classes.
+    reclassFieldLULC = "Value"
 
-# Set the reclassification table as a list of tuples, based on the reclassification table in the instruction document.
-remapLULC = RemapValue([[1,0],[2,0],[3,2],[4,3],[5,3],[6,3],[7,3],[8,0],[9,0],[10,1],[11,0],[12,1],[13,0],[14,0],[15,0],[16,2],[17,1],[18,1],[19,1],[20,0],[21,0]])
+    # Set the reclassification table as a list of tuples, based on the reclassification table in the instruction document.
+    remapLULC = RemapValue([[1,0],[2,0],[3,2],[4,3],[5,3],[6,3],[7,3],[8,0],[9,0],[10,1],[11,0],[12,1],[13,0],[14,0],[15,0],[16,2],[17,1],[18,1],[19,1],[20,0],[21,0]])
 
-# Execute Reclassify tool
-outReclassifyLULC = Reclassify(inRasterLULC, reclassFieldLULC, remapLULC, "NODATA")
+    # Execute Reclassify tool
+    outReclassifyLULC = Reclassify(inRasterLULC, reclassFieldLULC, remapLULC, "NODATA")
 
-# Save the output 
-outReclassifyLULC.save("outReclassifyLULC.tif")
+    # Save the output 
+    outReclassifyLULC.save("outReclassifyLULC.tif")
+
+LULC_Reclassify()
+
+def Mosaic_DEM():  #Mosaic the DEM tiles to a New Raster covering only the AOI polygon area.
+    
+    # Set the extent of the processing environment using a feature class, defined by the user and saved in 'InputDataAOI' folder as a shapefile 'HLS_AOI.shp'.
+    arcpy.env.extent = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\InputDataAOI\HLS_AOI.shp"
+        
+    # Set processing environment, matching the folder location of the input DEM raster tiles 'InputDataDEM'.
+    arcpy.env.workspace = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\InputDataDEM"
+
+    # Get and print a list of TIFs from the workspace
+    inputDEMs = arcpy.ListRasters("*", "TIF")
+    for raster in inputDEMs:
+            print(raster)
+
+    # Set output raster folder location:
+    outputDEM_Folder = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763"
+
+    # Set Ouput DEM file name:
+    outputDEM_File = "HLS_AOI_2m_DEM.tif"
+
+    # Set coordinate system to match the input DEMs:
+    DEM_CoordinateSystem = r"C:\Users\Lieutenant\EGM722_Coursework_AlexMason_B01039763\BNG.prj" # the .prj file is provided in the github repository, specifically for British National Grid.
+
+    # Set pixel type, matching the National LiDAR Programme input tiles, 32 bit float.
+    pixel_type = "32_BIT_FLOAT"
+
+    # Set the cell size of the output raster, to match the input DEM tiles, which for these dataset(s) is 2m pixels:
+    cellsize = "2"
+
+    # Set the number of bands for the output raster, just one, to record the elevation values:
+    number_of_bands = "1"
+
+    # Set the mosic method for the operation:
+    mosaic_method = "LAST" # this is also the default option for the tool.
+    
+    # Set the Mosaic Colourmap Mode:
+    mosaic_colormap_mode = "LAST" # This is the default for the tool.
+
+    # Mosaic several TIFF images to a new TIFF image:
+    arcpy.management.MosaicToNewRaster(inputDEMs, outputDEM_Folder, outputDEM_File, DEM_CoordinateSystem, pixel_type, cellsize, number_of_bands, mosaic_method, mosaic_colormap_mode)
+
+Mosaic_DEM() # Run the Mosaic DEM function.
+
+    
 
 
 
